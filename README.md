c79271966b7131dc09332deb85783711892b459b
lucasLang0 <lucas.lang@gmail.com>
2025-04-21 20:11:04 -0700

    or other

--- README.md
+++ README.md
@@ -280,9 +280,9 @@ Ensure that your runtimes on the lambda server are recorded below.
 
 |                        | elapsed time (sequential) | elapsed time (parallel)   |
 | -----------------------| ------------------------- | ------------------------- |
-| `pg_normalized`        |                           |                           | 
-| `pg_normalized_batch`  |                           |                           | 
-| `pg_denormalized`      |                           |                           | 
+| `pg_normalized`        |     4m20.320s             |                           | 
+| `pg_normalized_batch`  |     0m23.469s             |                           | 
+| `pg_denormalized`      |     0m22.794s             |                           | 
 
 Then upload a link to your forked github repo on sakai.
 
--- load_tweets_parallel.sh
+++ load_tweets_parallel.sh
@@ -6,13 +6,18 @@ echo '==========================================================================
 echo 'load pg_denormalized'
 echo '================================================================================'
 # FIXME: implement this with GNU parallel
+echo "$files" | time parallel sh load_denormalized.sh
+
 
 echo '================================================================================'
 echo 'load pg_normalized'
 echo '================================================================================'
 # FIXME: implement this with GNU parallel
+echo "$files" | time parallel python3 -u load_tweets.py --db=postgresql://postgres:pass@localhost:1051/ --inputs {}
+
 
 echo '================================================================================'
 echo 'load pg_normalized_batch'
 echo '================================================================================'
 # FIXME: implement this with GNU parallel
+echo "$files" | time parallel python3 -u load_tweets_batch.py --db=postgresql://postgres:pass@localhost:1052/ --inputs {}
